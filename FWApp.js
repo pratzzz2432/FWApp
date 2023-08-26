ceb77dd026cded7ad8cbc85d482f0733a3a24f67
Pratiksha <pratzzz2432@gmail.com>
2023-08-26 09:19:40 +0530

    updated api

--- FWApp.js
+++ FWApp.js
@@ -8,6 +8,7 @@ function formatDate(timestamp) {
 	if (minutes < 10) {
 	  minutes = `0${minutes}`;
 	}
+  
 	let days = [
 	  "Sunday",
 	  "Monday",
@@ -20,15 +21,20 @@ function formatDate(timestamp) {
 	let day = days[date.getDay()];
 	return `${day} ${hours}:${minutes}`;
   }
+  
   function formatDay(timestamp) {
 	let date = new Date(timestamp * 1000);
 	let day = date.getDay();
 	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
 	return days[day];
   }
+  
   function displayForecast(response) {
 	let forecast = response.data.daily;
+  
 	let forecastElement = document.querySelector("#forecast");
+  
 	let forecastHTML = `<div class="row">`;
 	forecast.forEach(function (forecastDay, index) {
 	  if (index < 6) {
@@ -56,15 +62,17 @@ function formatDate(timestamp) {
 	`;
 	  }
 	});
+  
 	forecastHTML = forecastHTML + `</div>`;
 	forecastElement.innerHTML = forecastHTML;
   }
+  
   function getForecast(coordinates) {
-	console.log(coordinates);
 	let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(displayForecast);
   }
+  
   function displayTemperature(response) {
 	let temperatureElement = document.querySelector("#temperature");
 	let cityElement = document.querySelector("#city");
@@ -74,7 +82,7 @@ function formatDate(timestamp) {
 	let dateElement = document.querySelector("#date");
 	let iconElement = document.querySelector("#icon");
   
-	celsiusTemperature = response.data.main.temp;
+	let celsiusTemperature = response.data.main.temp;
   
 	temperatureElement.innerHTML = Math.round(celsiusTemperature);
 	cityElement.innerHTML = response.data.name;
@@ -87,38 +95,23 @@ function formatDate(timestamp) {
 	  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
 	iconElement.setAttribute("alt", response.data.weather[0].description);
+  
 	getForecast(response.data.coord);
   }
+  
   function search(city) {
 	let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(displayTemperature);
   }
+  
   function handleSubmit(event) {
 	event.preventDefault();
 	let cityInputElement = document.querySelector("#city-input");
 	search(cityInputElement.value);
   }
-  function displayFahrenheitTemperature(event) {
-	event.preventDefault();
-	let temperatureElement = document.querySelector("#temperature");
-	celsiusLink.classList.remove("active");
-	fahrenheitLink.classList.add("active");
-	let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-	temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-  }
-  function displayCelsiusTemperature(event) {
-	event.preventDefault();
-	celsiusLink.classList.add("active");
-	fahrenheitLink.classList.remove("active");
-	let temperatureElement = document.querySelector("#temperature");
-	temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
-  let celsiusTemperature = null;
+  
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-  search("New York");
+  
+  search("Ranchi");
